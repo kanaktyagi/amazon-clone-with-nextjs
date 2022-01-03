d80da75b8e6e983b83e1b10108a28de0cba5234e
kanaktyagi <tyagiknk.india@gmail.com>
2022-01-03 15:18:26 +0530

    Added the banner for the amazon home page

---
+++ components/Banner.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import {Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+function Banner() {
+    return (
+        <div className='relative'>
+        <div className='absolute w-full h-32 bg-gradient-to-t from-gray-100 to-transparent bottom-0 z-20'/>
+           <Carousel
+            autoPlay
+            infiniteLoop
+            showStatus={false}
+            showIndicators={false}
+            showThumbs={false}
+            interval={3000}
+           >
+                 <div>
+                 <img loading='lazy' src="https://links.papareact.com/gi1" alt="banner_iamge_1"/>
+               
+                 </div>
+                 <div>
+                 <img loading='lazy' src="https://links.papareact.com/6ff" alt="banner_iamge_2"/>
+               
+                 </div>
+                 <div >
+                 <img loading='lazy' src="https://links.papareact.com/7ma" alt="banner_iamge_3"/>
+               
+                 </div>
+               
+           </Carousel>
+        </div>
+    )
+}
+
+export default Banner
